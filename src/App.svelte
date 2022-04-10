7117ef54cc317cd887c300db144fc60aaaf1a122
David Hide <d.hide@live.co.uk>
2022-04-10 12:00:14 +0100

    Add Teams component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,13 @@
 <script>
 	import AddPlayer from './AddPlayer.svelte';
 	import PlayerPool from './PlayerPool.svelte';
+	import Teams from './Teams.svelte';
 
-	let players = [];
+	let players = [
+		'David H',
+		'Max',
+		'Matt',
+	];
 
 	function handleSubmit(data) {
 		players = [...players, data.detail.name];
@@ -12,19 +17,19 @@
 <main>
 	<AddPlayer on:submit={handleSubmit} />
 	<PlayerPool bind:players={players} />
+	<Teams />
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 720px;
 		margin: 0 auto;
 	}
 
-	@media (min-width: 640px) {
+	/* @media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
-	}
+	} */
 </style>
---
+++ src/Teams.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<div class="teams">
+	<div class="teams__team">
+		<div class="teams__label">Team A</div>
+		<div class="teams__playerbox">
+			<ul>
+				<li>David H</li>
+				<li>Max</li>
+				<li>Matt</li>
+			</ul>
+		</div>
+	</div>
+	<div class="teams__team">
+		<div class="teams__label">Team B</div>
+		<div class="teams__playerbox">
+			
+		</div>
+	</div>
+</div>
+
+<style>
+	.teams {
+		display: flex;
+	}
+
+	.teams__team {
+		border: 1px solid #707070;
+	}
+
+	.teams__label {
+		font-size: 20px;
+		font-weight: 800;
+	}
+</style>
