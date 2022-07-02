8ce8b52b60f6036b5e77778c20436dcb1de64df5
David Hide <d.hide@live.co.uk>
2022-07-02 12:55:13 +0100

    Remove old player transitions

--- src/PlayerPool.svelte
+++ src/PlayerPool.svelte
@@ -1,22 +1,21 @@
 <script>
-	import { slide } from 'svelte/transition';
 	export let players;
 	export let teamsPicked;
 </script>
 
-<div transition:slide>
+<div>
 	<div class="heading-2">Player Pool</div>
 	{#if players.filter(player => !player.isPlaying).length}	
 		{#each players as player}
 			{#if !player.isPlaying}	
-			<div transition:slide|local>
-				{#if teamsPicked}
-					<p>{player.name} ({player.elo})</p>
-				{:else}
-					<input type="checkbox" name="{player.uid}" id="{player.uid}" on:change>
-					<label for="{player.uid}">{player.name} ({player.elo})</label>
-				{/if}
-			</div>
+				<div>
+					{#if teamsPicked}
+						<p>{player.name} ({player.elo})</p>
+					{:else}
+						<input type="checkbox" name="{player.uid}" id="{player.uid}" on:change>
+						<label for="{player.uid}">{player.name} ({player.elo})</label>
+					{/if}
+				</div>
 			{/if}
 		{/each}
 	{/if}
--- src/TeamsPool.svelte
+++ src/TeamsPool.svelte
@@ -1,23 +1,20 @@
 <script>
-	import { slide } from 'svelte/transition';
     export let players;
 	export let teamsPicked;
 </script>
 
 {#if !teamsPicked}
-	<div transition:slide>
-		<div class="teams__pool" >
-			{#if players.filter(player => player.isPlaying).length }
-				<div class="heading-2">Active players</div>
-				{#each players as player}
-					{#if player.isPlaying}
-						<div>
-							<input type="checkbox" name="{player.uid}" id="{player.uid}" checked on:change>
-							<label for="{player.uid}">{player.name} ({player.elo})</label>
-						</div>
-					{/if}
-				{/each}
-			{/if}
-		</div>
+	<div class="teams__pool" >
+		{#if players.filter(player => player.isPlaying).length }
+			<div class="heading-2">Active players</div>
+			{#each players as player}
+				{#if player.isPlaying}
+					<div>
+						<input type="checkbox" name="{player.uid}" id="{player.uid}" checked on:change>
+						<label for="{player.uid}">{player.name} ({player.elo})</label>
+					</div>
+				{/if}
+			{/each}
+		{/if}
 	</div>
 {/if}
