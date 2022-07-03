8ea37fcb2aba2d11d5dfd93b5c6e4ab883d1669e
David Hide <d.hide@live.co.uk>
2022-07-03 13:23:15 +0100

    Port PlayerSelection component over to fixture.svelte route component

--- src/routes/fixture.svelte
+++ src/routes/fixture.svelte
@@ -1,1 +1,94 @@
+<script>
+    import { fade } from 'svelte/transition';
+    import { crossfade } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
+
+    const [send, receive] = crossfade({
+        duration: 400,
+    });
+
+	let uid = 1;
+
+    // TODO get players from database
+	let players = [
+		{
+			uid: uid++,
+			name: 'David H',
+			isPlaying: false,
+			team: null,
+			elo: 1000,
+		},
+		{
+			uid: uid++,
+			name: 'Max',
+			isPlaying: false,
+			team: null,
+			elo: 2000,
+		},
+		{
+			uid: uid++,
+			name: 'Matt',
+			isPlaying: false,
+			team: null,
+			elo: 1500
+		},
+	];
+
+	$: playerPool = players.filter(player => !player.isPlaying);
+	$: selectedPlayers = players.filter(player => player.isPlaying);
+
+    /**
+     * @param {number} uid
+     * @param {boolean} isPlaying
+     */
+    function playerSelect(uid, isPlaying) {
+        players[uid - 1].isPlaying = isPlaying;
+    }
+</script>
+
 <h1>New Fixture</h1>
+<div class="player-selection">
+    <div class="players-container" >
+        {#if playerPool.length }
+            <div class="heading-2" transition:fade>Player Pool</div>
+            <div>
+                {#each playerPool as player (player)}
+                    <label class="block-label" for="{player.uid}" in:receive out:send animate:flip>
+                        <input type="checkbox" name="{player.uid}" id="{player.uid}" on:change={() => playerSelect(player.uid, true)}>
+                        {player.name} ({player.elo})
+                    </label>
+                {/each}
+            </div>
+        {/if}
+    </div>
+    <div class="players-container" >
+        {#if selectedPlayers.length }
+            <div class="heading-2" transition:fade>Active Players</div>
+            <div>
+                {#each selectedPlayers as player (player)}
+                    <label class="block-label" for="{player.uid}" in:receive out:send animate:flip>
+                        <input type="checkbox" name="{player.uid}" id="{player.uid}" checked on:change={() => playerSelect(player.uid, false)}>
+                        {player.name} ({player.elo})
+                    </label>
+                {/each}
+            </div>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .player-selection {
+		display: flex;
+		justify-content: space-between;
+		background: #f3f3f3;
+	}
+
+    .players-container {
+		width: 50%;
+		padding: 20px;
+	}
+
+    .block-label {
+        display: block;
+    }
+</style>
