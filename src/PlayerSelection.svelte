68a58353c1def77d52bdca0e4464856fda49d11a
David Hide <d.hide@live.co.uk>
2022-07-03 11:48:36 +0100

    Crossfade animation when moving selecting/unselecting players

--- src/PlayerSelection.svelte
+++ src/PlayerSelection.svelte
@@ -1,9 +1,15 @@
 <script>
     import { createEventDispatcher } from 'svelte';
+    import { fade } from 'svelte/transition';
+    import { crossfade } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
     export let players;
 	export let teamsPicked;
 
     const dispatch = createEventDispatcher();
+    const [send, receive] = crossfade({
+        duration: 400,
+    });
 
 	$: playerPool = players.filter(player => !player.isPlaying);
 	$: selectedPlayers = players.filter(player => player.isPlaying);
@@ -19,29 +25,29 @@
 <div class="player-selection">
         <div class="players-container" >
             {#if playerPool.length }
-                <div class="heading-2">Player Pool</div>
-                {#each playerPool as player}
-                    <div>
-                        {#if teamsPicked}
-                            <p>{player.name} ({player.elo})</p>
-                        {:else}
-                            <input type="checkbox" name="{player.uid}" id="{player.uid}" on:change={() => playerSelect(player.uid, true)}>
-                            <label for="{player.uid}">{player.name} ({player.elo})</label>
-                        {/if}
-                    </div>
-                {/each}
+                <div class="heading-2" transition:fade>Player Pool</div>
+                <div>
+                    {#each playerPool as player (player)}
+                            <label class="block-label" for="{player.uid}" in:receive out:send animate:flip>
+                                <input type="checkbox" name="{player.uid}" id="{player.uid}" on:change={() => playerSelect(player.uid, true)}>
+                                {player.name} ({player.elo})
+                            </label>
+                    {/each}
+                </div>
             {/if}
         </div>
     {#if !teamsPicked}
         <div class="players-container" >
             {#if selectedPlayers.length }
-                <div class="heading-2">Active Players</div>
-                {#each selectedPlayers as player}
-                    <div>
-                        <input type="checkbox" name="{player.uid}" id="{player.uid}" on:change={() => playerSelect(player.uid, false)}>
-                        <label for="{player.uid}">{player.name} ({player.elo})</label>
-                    </div>
-                {/each}
+                <div class="heading-2" transition:fade>Active Players</div>
+                <div>
+                    {#each selectedPlayers as player (player)}
+                        <label class="block-label" for="{player.uid}" in:receive out:send animate:flip>
+                            <input type="checkbox" name="{player.uid}" id="{player.uid}" checked on:change={() => playerSelect(player.uid, false)}>
+                            {player.name} ({player.elo})
+                        </label>
+                    {/each}
+                </div>
             {/if}
         </div>
     {/if}
@@ -58,4 +64,8 @@
 		width: 50%;
 		padding: 20px;
 	}
+
+    .block-label {
+        display: block;
+    }
 </style>
