1b4805db64e33f5d38b4cbc73331727da72003ec
David Hide <d.hide@live.co.uk>
2022-07-10 18:22:06 +0100

    Add layout with basic navigation and slot to render route content

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">Dashboard</a></li>
+        <li><a href="/fixture">Create fixture</a></li>
+        <li><a href="/players">Edit players</a></li>
+    </ul>
+</nav>
+<slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,1 @@
 <h1>Dashboard</h1>
-<a href="/fixture">Create fixture</a>
-<a href="/players">Edit players</a>
