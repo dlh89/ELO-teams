12de14691b9b0170ed15ed0dda011066f24deac3
David Hide <d.hide@live.co.uk>
2022-07-23 14:56:47 +0100

    Show different navbar for logged-in users

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,19 @@
+<script lang="ts">
+    import { session } from '$app/stores'
+
+    console.log($session.user);
+</script>
+
 <nav>
     <ul>
-        <li><a href="/dashboard">Dashboard</a></li>
-        <li><a href="/fixture">Create fixture</a></li>
-        <li><a href="/players">Edit players</a></li>
+        {#if !$session.user}
+            <li><a href="/">Home</a></li>
+            <li><a href="/login">Login</a></li>
+        {:else}
+            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href="/fixture">Create fixture</a></li>
+            <li><a href="/players">Edit players</a></li>
+        {/if}
     </ul>
 </nav>
 <slot></slot>
