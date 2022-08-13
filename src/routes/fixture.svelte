8f42e17f19446d751683d00fe7a0e27564e38372
David Hide <d.hide@live.co.uk>
2022-08-13 15:14:50 +0100

    Set and get players with firebase generated id (using push method)

--- src/routes/fixture.svelte
+++ src/routes/fixture.svelte
@@ -1,127 +1,161 @@
 <script context="module" lang="ts">
-    import type { Load } from '@sveltejs/kit';
-    import authStore from '../stores/authStore';
+	import type { Load } from '@sveltejs/kit';
+	import authStore from '../stores/authStore';
 
-    export const load: Load = ({ props }) => {
-        let shouldRedirect = false;
-        authStore.subscribe(async ({ isLoggedIn }) => {
-            if (!isLoggedIn ) {
-                shouldRedirect = true;
-            }
-        });
+	export const load: Load = ({ props }) => {
+		let shouldRedirect = false;
+		authStore.subscribe(async ({ isLoggedIn }) => {
+			if (!isLoggedIn) {
+				shouldRedirect = true;
+			}
+		});
 
-        if (shouldRedirect) {
-            return {
-                status: 302,
-                redirect: '/login',
-            }
-        }
+		if (shouldRedirect) {
+			return {
+				status: 302,
+				redirect: '/login',
+			};
+		}
 
-		return { props }
-	}
+		return { props };
+	};
 </script>
 
 <script>
-    import { fade } from 'svelte/transition';
-    import { crossfade } from 'svelte/transition';
-    import { flip } from 'svelte/animate';
-    import { teamsPicked } from '../stores/teamsStore';
-    import Teams from '../components/Teams.svelte';
-    import { getRandomTeams } from '../lib/helpers';
-    import { database } from '../config/firebase.js';
-    import { ref, onValue } from "firebase/database";
+	import { fade } from 'svelte/transition';
+	import { crossfade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	import { teamsPicked } from '../stores/teamsStore';
+	import Teams from '../components/Teams.svelte';
+	import { getRandomTeams } from '../lib/helpers';
+	import { database } from '../config/firebase.js';
+	import { ref, onValue } from 'firebase/database';
 
-    const [send, receive] = crossfade({
-        duration: 200,
-    });
+	const [send, receive] = crossfade({
+		duration: 200,
+	});
 
 	let players = [];
-    const playersRef = ref(database, 'players');
+	const playersRef = ref(database, 'players');
 
-    onValue(playersRef, (snapshot) => {
-        const data = snapshot.val();
+	onValue(playersRef, (snapshot) => {
+		const data = snapshot.val();
 
-        players = Object.keys(data).map((player, i) => {
-            data[player].uid = i;
-            data[player].isPlaying = false;
-            data[player].team = null;
-            return data[player];
-        });
-    });
+		const playerIds = Object.keys(data);
+		players = playerIds.map((id) => {
+			return {
+				id,
+				...data[id],
+				isPlaying: false,
+				team: null,
+			};
+		});
+	});
 
-    $: playerPool = players.filter(player => !player.isPlaying);
-	$: selectedPlayers = players.filter(player => player.isPlaying);
+	$: playerPool = players.filter((player) => !player.isPlaying);
+	$: selectedPlayers = players.filter((player) => player.isPlaying);
 
-    /**
-     * @param {number} uid
-     * @param {boolean} isPlaying
-     */
-    function playerSelect(uid, isPlaying) {
-        players[uid].isPlaying = isPlaying;
-    }
+	/**
+	 * @param {number} id
+	 * @param {boolean} isPlaying
+	 */
+	function playerSelect(id, isPlaying) {
+		const player = players.find((player) => player.id === id);
+		if (player) {
+			player.isPlaying = isPlaying;
+			players = [...players];
+		}
+	}
 
-    function handleSortTeams() {
-        // Sort randomly for now
-        const randomTeams = getRandomTeams(selectedPlayers.length);
+	function handleSortTeams() {
+		// Sort randomly for now
+		const randomTeams = getRandomTeams(selectedPlayers.length);
 
-		selectedPlayers.forEach(function(player, i) {
+		selectedPlayers.forEach(function (player, i) {
 			player.team = randomTeams[i];
 		});
 
-        players = [...players];
-        teamsPicked.set(true);
-    }
+		players = [...players];
+		teamsPicked.set(true);
+	}
 </script>
 
 <div class="row">
-    <h1>New Fixture</h1>
-    <div class="player-selection">
-        <div class="players-container" >
-            {#if playerPool.length }
-                <div class="heading-2" transition:fade|local>Player Pool</div>
-                <div>
-                    {#each playerPool as player (player)}
-                        <label class="block-label" for="{player.uid}" in:receive out:send animate:flip>
-                            <input type="checkbox" name="{player.uid}" id="{player.uid}" on:change={() => playerSelect(player.uid, true)} disabled="{$teamsPicked}">
-                            {player.name} ({player.elo})
-                        </label>
-                    {/each}
-                </div>
-            {/if}
-        </div>
-        <div class="players-container" >
-            {#if selectedPlayers.length }
-                <div class="heading-2" transition:fade|local>Active Players</div>
-                <div>
-                    {#each selectedPlayers as player (player)}
-                        <label class="block-label" for="{player.uid}" in:receive out:send animate:flip>
-                            <input type="checkbox" name="{player.uid}" id="{player.uid}" checked on:change={() => playerSelect(player.uid, false)} disabled="{$teamsPicked}">
-                            {player.name} ({player.elo})
-                        </label>
-                    {/each}
-                </div>
-            {/if}
-        </div>
-    </div>
-    {#if !$teamsPicked && selectedPlayers.length > 1}
-        <button on:click="{handleSortTeams}">Sort teams</button>
-    {/if}
-    <Teams teamsPlayers={selectedPlayers} on:click={handleSortTeams} />
+	<h1>New Fixture</h1>
+	<div class="player-selection">
+		<div class="players-container">
+			{#if playerPool.length}
+				<div class="heading-2" transition:fade|local>Player Pool</div>
+				<div>
+					{#each playerPool as player (player)}
+						<label
+							class="block-label"
+							for={player.id}
+							in:receive
+							out:send
+							animate:flip
+						>
+							<input
+								type="checkbox"
+								name={player.id}
+								id={player.id}
+								on:change={() => playerSelect(player.id, true)}
+								disabled={$teamsPicked}
+							/>
+							{player.name} ({player.elo})
+						</label>
+					{/each}
+				</div>
+			{/if}
+		</div>
+		<div class="players-container">
+			{#if selectedPlayers.length}
+				<div class="heading-2" transition:fade|local>
+					Active Players
+				</div>
+				<div>
+					{#each selectedPlayers as player (player)}
+						<label
+							class="block-label"
+							for={player.id}
+							in:receive
+							out:send
+							animate:flip
+						>
+							<input
+								type="checkbox"
+								name={player.id}
+								id={player.id}
+								checked
+								on:change={() => playerSelect(player.id, false)}
+								disabled={$teamsPicked}
+							/>
+							{player.name} ({player.elo})
+						</label>
+					{/each}
+				</div>
+			{/if}
+		</div>
+	</div>
+	{#if !$teamsPicked && selectedPlayers.length > 1}
+		<button on:click={handleSortTeams}>Sort teams</button>
+	{/if}
+	<Teams teamsPlayers={selectedPlayers} on:click={handleSortTeams} />
 </div>
 
 <style>
-    .player-selection {
+	.player-selection {
 		display: flex;
 		justify-content: space-between;
 		background: #f3f3f3;
 	}
 
-    .players-container {
+	.players-container {
 		width: 50%;
 		padding: 20px;
 	}
 
-    .block-label {
-        display: block;
-    }
+	.block-label {
+		display: block;
+	}
 </style>
--- src/routes/players/index.svelte
+++ src/routes/players/index.svelte
@@ -1,77 +1,86 @@
 <script context="module" lang="ts">
-    import type { Load } from '@sveltejs/kit';
-    import authStore from '../../stores/authStore';
+	import type { Load } from '@sveltejs/kit';
+	import authStore from '../../stores/authStore';
 	import CalculateElo from '../../lib/CalculateElo.js';
-    import { database } from '../../config/firebase.js';
-    import { ref, set, onValue } from "firebase/database";
+	import { database } from '../../config/firebase.js';
+	import { ref, push, onValue } from 'firebase/database';
 
-    export const load: Load = ({ props }) => {
-        let shouldRedirect = false;
-        authStore.subscribe(async ({ isLoggedIn }) => {
-            if (!isLoggedIn) {
-                shouldRedirect = true;
-            }
-        });
+	export const load: Load = ({ props }) => {
+		let shouldRedirect = false;
+		authStore.subscribe(async ({ isLoggedIn }) => {
+			if (!isLoggedIn) {
+				shouldRedirect = true;
+			}
+		});
 
-        if (shouldRedirect) {
-            return {
-                status: 302,
-                redirect: '/login',
-            }
-        }
+		if (shouldRedirect) {
+			return {
+				status: 302,
+				redirect: '/login',
+			};
+		}
 
-		return { props }
-	}
+		return { props };
+	};
 </script>
 
 <script>
-    let players = [];
-	let uid;
-    const playersRef = ref(database, 'players');
+	let players = [];
+	const playersRef = ref(database, 'players');
 
-    onValue(playersRef, (snapshot) => {
-        const data = snapshot.val();
-        players = data;
-        uid = data && data.length ? data.length : 0;
-    });
+	onValue(playersRef, (snapshot) => {
+		const data = snapshot.val();
+		const playerIds = Object.keys(data);
+		players = playerIds.map((id) => {
+			return {
+				id,
+				...data[id],
+			};
+		});
+	});
 
 	const calculateElo = new CalculateElo();
 
-    function handleSubmit(e) {
-        e.preventDefault();
+	function handleSubmit(e) {
+		e.preventDefault();
 		const formData = new FormData(e.target);
 
-        set(ref(database, 'players/' + uid++), {
-            name: formData.get('playerName'),
-            elo: calculateElo.startingElo,
-        });
+		push(ref(database, 'players/'), {
+			name: formData.get('playerName'),
+			elo: calculateElo.startingElo,
+		});
 
 		e.target.reset();
 	}
 </script>
 
 <div class="row">
-    <h1>Players</h1>
-    <div>
-        <form on:submit={handleSubmit}>
-            <label for="player js-add-player-btn">Add a player:</label>
-            <input type="text" id="player" name="playerName" placeholder="Player name">
-            <input type="submit" value="Add player">
-        </form>
-    </div>
-    <div>
-        {#if players && players.length }
-            <div>
-                <ul>
-                    {#each players as {name, elo}, i}
-                        {#if name}
-                            <li>
-                                <a href="/players/{i}">{name} ({elo})</a>
-                            </li>
-                        {/if}
-                    {/each}
-                </ul>
-            </div>
-        {/if}
-    </div>
+	<h1>Players</h1>
+	<div>
+		<form on:submit={handleSubmit}>
+			<label for="player js-add-player-btn">Add a player:</label>
+			<input
+				type="text"
+				id="player"
+				name="playerName"
+				placeholder="Player name"
+			/>
+			<input type="submit" value="Add player" />
+		</form>
+	</div>
+	<div>
+		{#if players && players.length}
+			<div>
+				<ul>
+					{#each players as { id, name, elo }}
+						{#if name}
+							<li>
+								<a href="/players/{id}">{name} ({elo})</a>
+							</li>
+						{/if}
+					{/each}
+				</ul>
+			</div>
+		{/if}
+	</div>
 </div>
