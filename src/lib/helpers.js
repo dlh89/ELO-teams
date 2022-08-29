e73312ce3827ebf14eb0a52f52e08974788fda67
David Hide <d.hide@live.co.uk>
2022-08-29 21:49:27 +0100

    Add helper functions to get current date and time strings

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,46 +3,74 @@
  * @param items
  */
 const fisherYatesShuffle = (items) => {
-    let currentIndex = items.length, currentItem, randomIndex;
+	let currentIndex = items.length,
+		currentItem,
+		randomIndex;
 
-    // While there remain elements to shuffle…
-    while (currentIndex) {
+	// While there remain elements to shuffle…
+	while (currentIndex) {
+		// Pick a remaining element…
+		randomIndex = Math.floor(Math.random() * currentIndex--);
 
-        // Pick a remaining element…
-        randomIndex = Math.floor(Math.random() * currentIndex--);
+		// And swap it with the current element.
+		currentItem = items[currentIndex];
+		items[currentIndex] = items[randomIndex];
+		items[randomIndex] = currentItem;
+	}
 
-        // And swap it with the current element.
-        currentItem = items[currentIndex];
-        items[currentIndex] = items[randomIndex];
-        items[randomIndex] = currentItem;
-    }
-
-    return items;
-}
+	return items;
+};
 
 /**
  * Randomly sort an equal (or as close as possible) number of 'a' and 'b' values
- * 
+ *
  * @param {number} playerCount Number of players to sort into teams
  * @returns {Array} Randomly sorted array with values of a/b
  */
 const getRandomTeams = (playerCount) => {
-    let teams = [];
-    let aTotal = Math.round(playerCount / 2);
-    let aCount = 0;
+	let teams = [];
+	let aTotal = Math.round(playerCount / 2);
+	let aCount = 0;
 
-    for (let i = 0; i < playerCount; i++) {
-        if (aCount < aTotal) {
-            aCount++;
-            teams.push('a');
-        } else {
-            teams.push('b');
-        }
-    }
+	for (let i = 0; i < playerCount; i++) {
+		if (aCount < aTotal) {
+			aCount++;
+			teams.push('a');
+		} else {
+			teams.push('b');
+		}
+	}
 
-    teams = fisherYatesShuffle(teams);
+	teams = fisherYatesShuffle(teams);
 
-    return teams;
-}
+	return teams;
+};
 
-export { fisherYatesShuffle, getRandomTeams }
+const getCurrentDateString = () => {
+	// 1970-10-31 format
+	const nowDate = new Date();
+
+	const currentDateString = `${nowDate.getFullYear()}-${String(
+		nowDate.getMonth() + 1
+	).padStart(2, '0')}-${nowDate.getDate()}`;
+
+	return currentDateString;
+};
+
+const getCurrentTimeString = () => {
+	// 00:00:00 format
+	const nowDate = new Date();
+
+	const currentTimeString = `${nowDate.getHours()}:${nowDate.getMinutes()}:${String(
+		nowDate.getSeconds()
+	).padStart(2, '0')}`;
+
+	return currentTimeString;
+};
+
+export {
+	fisherYatesShuffle,
+	getRandomTeams,
+	getCurrentDateString,
+	getCurrentTimeString,
+};
